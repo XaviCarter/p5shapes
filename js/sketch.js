b763403c6e84927ed21a940a9f25ef0ddba44fad
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-10-28 12:26:41 +0100

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,13 @@
+//setup function runs once at start
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
 }
 
+//the draw function runs 60 times per second in a continuous loop
 function draw() {
-    background(220);
-
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    background(220); //greyscale colour for background
+    
+    fill(0, 0, 0);
+    square(100, 100, 400,);
 }
